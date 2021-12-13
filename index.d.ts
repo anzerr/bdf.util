0537066de66e096f16ab70be1c11b958aaef03c3
anzerr <thoron174+anzerr@gmail.com>
2021-12-13 21:37:45 +0100

    🚧 dump

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,20 @@
 
+class Char {
+
+	offset: {x: number; y: number};
+	width: number;
+	height: number;
+	bitmap: number[];
+
+}
+
+type CharList = {[key: string]: Char};
+
 declare namespace bdf {
 
-	function parse(data: string | Buffer) : any
-	function bitmap() : Promise<any>
-	function bitmapSync(data: string | Buffer) : any
+	function parse(data: string | Buffer): any
+	function bitmap(string: string): Promise<CharList>
+	function bitmapSync(data: string | Buffer): CharList
 
 }
 
